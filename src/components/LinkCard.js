d2017fcdea03f60f6938b143a6e37028b2e6c73e
kmylo6 <kmylo6@gmail.com>
2021-07-19 06:25:29 -0300

    all changed

--- src/components/LinkCard.js
+++ src/components/LinkCard.js
@@ -1,46 +1,50 @@
-import React from 'react';
+import React from "react";
 
 export default function LinkCard({ link, refreshLinks }) {
-    const archiveLink = async () => {
-        link.archived = true;
-        try {
-            await fetch('/.netlify/functions/updateLink', {
-                method: 'PUT',
-                body: JSON.stringify(link),
-            });
-            refreshLinks();
-        } catch (error) {
-            console.error('AHHH', error);
-        }
-    };
+  const archiveLink = async () => {
+    link.archived = true;
+    try {
+      await fetch("/.netlify/functions/updateLink", {
+        method: "PUT",
+        body: JSON.stringify(link),
+      });
+      refreshLinks();
+    } catch (error) {
+      console.error("AHHH", error);
+    }
+  };
 
-    const deleteLink = async () => {
-        const id = link._id;
-        try {
-            await fetch('/.netlify/functions/deleteLink', {
-                method: 'DELETE',
-                body: JSON.stringify({ id }),
-            });
-            refreshLinks();
-        } catch (error) {
-            console.error('AHHH', error);
-        }
-    };
-    return (
-        <div className="card mb-3">
-            <div className="card-header">{link.name}</div>
-            <div className="card-body">
-                <a href={link.url}>{link.url}</a>
-                <p>{link.description}</p>
-            </div>
-            <div className="card-footer">
-                <button className="btn btn-warning mr-2" onClick={archiveLink}>
-                    Archive
-                </button>
-                <button className="btn btn-danger" onClick={deleteLink}>
-                    Delete
-                </button>
-            </div>
-        </div>
-    );
+  const deleteLink = async () => {
+    const id = link._id;
+    try {
+      await fetch("/.netlify/functions/deleteLink", {
+        method: "DELETE",
+        body: JSON.stringify({ id }),
+      });
+      refreshLinks();
+    } catch (error) {
+      console.error("AHHH", error);
+    }
+  };
+  return (
+    <div className="card mb-3">
+      <div className="card-header">{link.name}</div>
+      <div className="card-body">
+        <a href={link.url}>{link.url}</a>
+        <p>{link.description}</p>
+      </div>
+      <div className="card-footer">
+        <button className="btn btn-warning mr-2" onClick={archiveLink}>
+          Archive
+        </button>
+        <button className="btn btn-primary mr-2" disabled>
+          Edit
+        </button>
+        {/* TODO */}
+        <button className="btn btn-danger" onClick={deleteLink}>
+          Delete
+        </button>
+      </div>
+    </div>
+  );
 }
